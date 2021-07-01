2769e0d7e2b33d5f477d97484646d2167616fe65
RUSLAM JAYA <86702059+RUSLAMJAYA@users.noreply.github.com>
2021-07-01 15:15:28 +0700

    Update loadingpage.js

--- loadingpage.js
+++ loadingpage.js
@@ -1,17 +1,11 @@
         document.onreadystatechange = function() {
             if (document.readyState !== "complete") {
-                document.querySelector(
-                  "body").style.visibility = "hidden";
-				document.getElementById(
-                  "topheader").style.display = "none";
-                document.querySelector(
-                  "#loader").style.visibility = "visible";
+                document.querySelector("body").style.visibility = "hidden!important";
+		document.getElementById("topheader").style.display = "none";
+                document.querySelector("#loader").style.visibility = "visible";
             } else {
-                document.querySelector(
-                  "#loader").style.display = "none";
-                document.querySelector(
-                  "body").style.visibility = "visible";
-				document.getElementById(
-                  "topheader").style.display = "auto";				  
+                document.querySelector("#loader").style.display = "none";
+                document.querySelector("body").style.visibility = "visible!important";
+		document.getElementById("topheader").style.display = "auto";				  
             }
         };
