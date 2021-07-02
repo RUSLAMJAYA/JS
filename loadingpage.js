62fca726a0dce41e49be67fbda5c333686f658f8
RUSLAM JAYA <86702059+RUSLAMJAYA@users.noreply.github.com>
2021-07-02 16:01:02 +0700

    Add files via upload

--- loadingpage.js
+++ loadingpage.js
@@ -4,8 +4,10 @@
                 document.querySelector("body").style.visibility = "hidden";
 				document.querySelector("#loader").style.visibility = "visible";
             } else {
+				setTimeout(function(){
                 document.querySelector("#loader").style.display = "none";
                 document.querySelector("body").style.visibility = "visible";
-				document.querySelector(".dd01").style.visibility = "visible";    
+				document.querySelector(".dd01").style.visibility = "visible";
+				}, 9000);
             }
         };
