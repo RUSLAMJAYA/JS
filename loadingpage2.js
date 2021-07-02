03859a81d3e7a485231c2312a1dc4403f98e02a2
RUSLAM JAYA <86702059+RUSLAMJAYA@users.noreply.github.com>
2021-07-02 19:39:25 +0700

    Add files via upload

---
+++ loadingpage2.js
@@ -0,0 +1,17 @@
+        document.onreadystatechange = function() {
+            if (document.readyState !== "complete") {
+				document.querySelector("body").style.background-color = "black";
+				document.querySelector("#navigate2").style.top = "-70px";
+                document.querySelector("body").style.visibility = "hidden";
+				document.querySelector(".design-blogger").style.visibility = "hidden";
+				document.querySelector("#loader").style.visibility = "visible";
+            } else {
+				setTimeout(function(){
+                document.querySelector("#loader").style.display = "none";
+				document.querySelector("body").style.background-color = "white";
+				document.querySelector("#navigate2").style.top = "0px";
+				document.querySelector(".design-blogger").style.visibility = "visible";
+                document.querySelector("body").style.visibility = "visible";
+				}, 9000);
+            }
+        };
