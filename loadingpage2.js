f5b130948defab9490fb502e9198bc13b384b412
RUSLAM JAYA <86702059+RUSLAMJAYA@users.noreply.github.com>
2021-07-08 17:50:35 +0700

    Update loadingpage2.js

--- loadingpage2.js
+++ loadingpage2.js
@@ -4,12 +4,9 @@
 		document.querySelector("#preloading").style.visibility = "visible";
   		document.querySelector("#preloading").style.position  = "fixed";
             } else {
-		setTimeout(function(){
                 document.querySelector("#preloading").style.display = "none";
                 document.querySelector("#body").style.visibility = "visible";
   		document.querySelector("#body").style.height = "auto";
-  		document.querySelector("#body").style.backgroundColor  = "white";
     		document.querySelector("#preloading").style.position  = "relative";
-		}, 9000);
-            }
+		}
         }; 
