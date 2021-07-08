af8107a25aaf884e18419cac02fc8f2b0b880eec
RUSLAM JAYA <86702059+RUSLAMJAYA@users.noreply.github.com>
2021-07-08 18:23:50 +0700

    Update loadingpage2.js

--- loadingpage2.js
+++ loadingpage2.js
@@ -2,11 +2,8 @@
             if (document.readyState !== "complete") {
                 document.querySelector("#dvContainer").style.visibility = "hidden";
 		document.querySelector("#preloading").style.visibility = "visible";
-  		document.querySelector("#preloading").style.position  = "fixed";
             } else {
-                document.querySelector("#preloading").style.display = "none";
                 document.querySelector("#dvContainer").style.visibility = "visible";
-  		document.querySelector("#dvContainer").style.height = "auto";
-    		document.querySelector("#preloading").style.position  = "relative";
+		document.querySelector("#preloading").style.visibility = "hidden";
 		}
         }; 
